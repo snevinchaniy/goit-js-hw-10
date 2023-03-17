b3c2e27ce57e5f2da2676a6db4af539eb8cc73de
snevinchaniy <85674052+snevinchaniy@users.noreply.github.com>
2023-03-17 12:29:38 +0200

    added import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import Debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
